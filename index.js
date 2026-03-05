c86b95209cdc42c06931329ba432faf12c7dcb0c
sabelson <sabelson@mnccapital.com>
2026-03-05 18:43:44 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 app.post('/search', async (req, res) => {
   try {
