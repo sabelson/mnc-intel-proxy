fa27af6084dd1b53d340c4c46e013e4996005bc9
sabelson <sabelson@mnccapital.com>
2026-03-16 12:46:55 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// v2 
 const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
@@ -7,7 +6,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/search', async (req, res) => {
   try {
@@ -34,15 +33,15 @@ app.post('/search', async (req, res) => {
     let offset = 0;
     let keepGoing = true;
 
-    while (keepGoing) {
-      const r = await fetch('https://mnccapital.sugarondemand.com/rest/v11_1/Accounts?max_num=100&offset=' + offset + '&fields=name,industry,annual_revenue,billing_address_country,billing_address_city,ownership,date_modified,employees,description,account_type', {
+    while (keepGoing && all.length < 150) {
+      const r = await fetch('https://mnccapital.sugarondemand.com/rest/v11_1/Accounts?max_num=50&offset=' + offset + '&fields=name,industry,annual_revenue,billing_address_country,billing_address_city,ownership,date_modified,employees,description,account_type', {
         headers: { 'OAuth-Token': loginData.access_token }
       });
       const d = await r.json();
       if (!d.records || d.records.length === 0) break;
       all = all.concat(d.records);
-      if (d.records.length < 100) keepGoing = false;
-      offset += 100;
+      if (d.records.length < 50) keepGoing = false;
+      offset += 50;
     }
 
     const companies = all.map(function(a) {
@@ -53,8 +52,7 @@ app.post('/search', async (req, res) => {
         location: (a.billing_address_city || '') + ' ' + (a.billing_address_country || ''),
         ownership: a.ownership || a.account_type,
         lastModified: a.date_modified ? a.date_modified.split('T')[0] : '',
-        employees: a.employees,
-        description: a.description
+        employees: a.employees
       };
     });
 
